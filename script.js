019a0bb2e71f53781aca0013cf2daf396c6a6585
Milos252 <77508044+Milos252@users.noreply.github.com>
2022-05-02 01:16:00 +0200

    Update

--- script.js
+++ script.js
@@ -28,3 +28,17 @@ btnS.forEach((button) => {
         });
     });
 });
+
+let naslovDugme = document.querySelector('.header button');
+naslovDugme.addEventListener('click', () => {
+    let menu = document.querySelector('.header ul');
+    let btn = document.querySelector('.header button');
+
+    if (btn.innerText === 'MENU') {
+        menu.style.display = 'block';
+        btn.innerText = 'CLOSE';
+    } else {
+        menu.style.display = 'none';
+        btn.innerText = 'MENU';
+    }
+});
